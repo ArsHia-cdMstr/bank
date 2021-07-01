8a2e079390d41289908b3f237cb5ee261e28bb40
ai-shra <85890250+ai-shra@users.noreply.github.com>
2021-07-01 16:29:52 +0430

    adding gmail checker and adding users in the bank
     already have the sign in sign up and a piece of main pages

---
+++ src/sample/User.java
@@ -0,0 +1,24 @@
+package sample;
+
+public class User {
+    private String FirstName;
+    private String LastName;
+    private String Password;
+    private String NationalCode;
+    private String Email;
+    private String Phone;
+
+    public User(String firstName, String lastName, String password,
+                   String nationalCode, String email, String phone) {
+        FirstName = firstName;
+        LastName = lastName;
+        Password = password;
+        NationalCode = nationalCode;
+        Email = email;
+        Phone = phone;
+    }
+
+    
+
+
+}
